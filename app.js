f70be3b99fdada35fec94429f2b9c2beea0279a1
Harry Difolco <h@difol.co>
2020-03-15 06:22:27 +0000

    port 3000

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express"),
   app = express(),
   bodyParser = require("body-parser");
-port = process.env.PORT || 80;
+port = process.env.PORT || 3000;
 
 app.listen(port);
 
